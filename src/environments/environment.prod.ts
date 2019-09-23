493d386be028dc79ec5a8257117f2dae9e2d860b
pragathibaskar <47776430+pragathibaskar@users.noreply.github.com>
2019-09-23 10:38:50 +0530

    added gateway url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   supportedLanguages: [
     'es-ES'
   ],
-  serverUrl: 'http://cert-service-adifv2.apps.okd.openshifthub.ml',
+  serverUrl: 'http://gateway-adifv2.apps.okd.openshifthub.ml/api/micro3',
   security: 'csrf'
 };
