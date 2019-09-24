78bd5065e29d78b317a4fde39c178ef8735954e3
pragathibaskar <47776430+pragathibaskar@users.noreply.github.com>
2019-09-24 16:48:09 +0530

    added serviceUnavailable msg

--- src/app/shared/translation/translate_es.ts
+++ src/app/shared/translation/translate_es.ts
@@ -1,5 +1,6 @@
 export class TranslationES {
     static consultant_period = {
-        sorryForIncovinience: { action: 'error', msg: 'lo siento por incoviencia' }
+        sorryForIncovinience: { action: 'error', msg: 'lo siento por incoviencia' },
+        serviceUnAvailable: { action: 'error', msg: 'El servicio no está disponible. Por favor, inténtelo más tarde'}
     };
 }
