aceb16b23dc7892afab49ef1b7d181f23b60aa16
Anish Kumar <59729398+capanish@users.noreply.github.com>
2021-01-04 14:55:22 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   supportedLanguages: [
     'es-ES'
   ],
-  serverUrl: 'http://cert-service-adifv2.apps.okd.openshifthub.ml',
+  serverUrl: 'http://cert-service-adifv2.apps.okd.openshiftorigin.ml',
   security: 'csrf'
 };
